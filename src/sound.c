c91b2177153a5d6729407a522f8a04ec01abc143
celerizer <33245078+celerizer@users.noreply.github.com>
2020-02-09 13:12:58 -0600

    I was way overthinking how sound should work

--- src/sound.c
+++ src/sound.c
@@ -38,7 +38,7 @@ void sound_write()
    /* Fill any unwritten samples with the last known tone */
    if (last_tick != PF_SAMPLES - 1)
    {
-      for (i = last_tick + 1; i < PF_SAMPLES; i++)
+      for (i = last_tick; i < PF_SAMPLES; i++)
          frequencies[i] = frequency_last;
    }
 
@@ -63,8 +63,6 @@ void sound_write()
          amplitude *= PF_DECAY;
       }
    }
-   frequency_pushes = 0;
-   frequency_this_tick = 0;
    last_tick = 0;
 }
 
@@ -72,25 +70,13 @@ void sound_push_back(u8 frequency, u32 current_cycles, u32 total_cycles)
 {
    u16 current_tick = PF_SAMPLES * ((float)current_cycles / (float)total_cycles);
 
-   if (last_tick == 0)
+   if (current_tick != last_tick)
    {
       u16 i;
 
-      for (i = 0; i <= current_tick; i++)
+      for (i = last_tick; i < current_tick; i++)
          frequencies[i] = frequency_last;
    }
-   else if (current_tick != last_tick)
-   {
-      u16 i;
-
-      frequency_this_tick = ((float)frequency_this_tick / (float)frequency_pushes);
-      for (i = last_tick; i <= current_tick; i++)
-         frequencies[i] = frequency_this_tick;
-      frequency_this_tick = 0;
-      frequency_pushes = 0;
-   }
-   frequency_this_tick += SOUND_FREQUENCIES[frequency];
-   frequency_pushes++;
    last_tick = current_tick;
    frequency_last = SOUND_FREQUENCIES[frequency];
 }
