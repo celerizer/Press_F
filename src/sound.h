a64d8962dfb845105644a3b29d5111fc4759726a
celerizer <33245078+celerizer@users.noreply.github.com>
2024-01-10 20:29:56 -0600

    init

--- src/sound.h
+++ src/sound.h
@@ -8,10 +8,4 @@
 #define PF_PERIOD  1.0f / PF_FREQUENCY
 #define PF_SAMPLES PF_FREQUENCY / 60
 
-void sound_empty();
-void sound_push_back(u8 frequency, u32 current_cycles, u32 total_cycles);
-void sound_write();
-
-extern i16 samples[PF_SAMPLES * 2];
-
 #endif
