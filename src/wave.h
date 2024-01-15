0eaa1e26ca29da2c9a840f5904e9f84472e0060c
celerizer <33245078+celerizer@users.noreply.github.com>
2024-01-15 16:06:12 -0600

    buildfix

--- src/wave.h
+++ src/wave.h
@@ -4,7 +4,9 @@
 #include "types.h"
 
 #define PF_PI 3.141592653589793115997963468544185161590576171875
+
 /* Largest Taylor series factorial a float can hold */
+#define PF_TERMS 32
 
 double pf_wave(double x, u8 cosine);
 
